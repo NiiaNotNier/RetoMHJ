7f87afc89c32bbd34add9b2beaeb49b310888b9c
a24956 <a24956@svalero.com>
2021-12-06 23:25:23 +0100

    añadido metodo orderproducto para el ejercicio 3

--- RetoBackend/RetoBackend/src/main/java/com/example/demo/Controllers/OrderProductController.java
+++ RetoBackend/RetoBackend/src/main/java/com/example/demo/Controllers/OrderProductController.java
@@ -1,8 +1,40 @@
 package com.example.demo.Controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.demo.Models.ElementNotFoundException;
+import com.example.demo.Models.OrderProduct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderProductController {
+
+    public static ArrayList<OrderProduct> order = new ArrayList(Arrays.asList(
+            new OrderProduct(1, "Teclado", 3),
+            new OrderProduct(2, "Proyector", 2)));
+
+    @GetMapping("OrderProduct")
+    public List<OrderProduct> GetAllOrderProduct() {
+        return order;
+    }
+
+    @GetMapping("OrderProduct/{idPedido}")
+    public static OrderProduct GetPedidosById(@PathVariable("idPedido") int idPedido) {
+        return FindOrderProductById(idPedido);
+    }
+
+    public static OrderProduct FindOrderProductById(int idPedido) {
+        for (OrderProduct op : order) {
+            if (op.getIdPedido() == idPedido) {
+                return op;
+            }
+        }
+        throw new ElementNotFoundException();
+    }
 
 }
