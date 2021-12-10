511b02a3028baff9d7551857c276b621ff25f809
a24956 <a24956@svalero.com>
2021-12-10 20:22:47 +0100

    añadidas variables para el save

--- RetoBackend/RetoBackend/src/main/java/com/example/MyListProductosController.java
+++ RetoBackend/RetoBackend/src/main/java/com/example/MyListProductosController.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class MyListProductosController {
         return productosMyList;
     }
 
-    @GetMapping("/myList/add/{idProducto}")
+    @PostMapping("/myList/add/{idProducto}")
     public Productos add(@PathVariable("idProducto") int idProducto) {
         // compartimos nuestro listado para coger la info de ese metodo
         Productos p = ProductosController.FindProductoByIdProducto(idProducto);
